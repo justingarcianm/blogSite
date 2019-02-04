270fb3bc4772fb0f3aeac1927fea31fd12016c47
justin garcia <justinframedev@gmail.com>
2019-02-04 12:19:28 -0700

    add show page template and edit form

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const express = require("express"),
     app = express(),
     port = 3000,
     bodyParser = require("body-parser"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    methodOverride = require("method-override")
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-mongoose.connect("mongodb://localhost/story");
+app.use(methodOverride("_method"))
+mongoose.connect("mongodb://localhost:27017/story", { useNewUrlParser: true });
 
 // SCHEMA SETUP
 
@@ -26,18 +28,21 @@ const Article = mongoose.model("Article", articleSchema);
 // =======================================================
 //  ROUTES
 // =======================================================
-app.get('/', (req, res) => {
+
+// Index Route - show all storys
+app.get('/blog', (req, res) => {
     // Get all articles from db
     Article.find({}, (err, allArticles) => {
         if (err) {
             console.log(err)
         } else {
-            res.render("home", { allArticles })
+            res.render("index", { allArticles })
         }
     })
 });
 
-app.post("/", (req, res) => {
+// Create Route - add new article to DB
+app.post("/blog", (req, res) => {
     // get data from form and add to newArticle array
     const author = req.body.author;
     const image = req.body.image;
@@ -50,18 +55,55 @@ app.post("/", (req, res) => {
             console.log(err)
         } else {
             // redirect back to home page
-            res.redirect("/");
+            res.redirect("/blog");
         }
     })
 })
 
-app.get('/article/new', (req, res) => {
+// new Route - show form to create new article
+app.get('/blog/new', (req, res) => {
     res.render("newarticle")
 })
 
+// Edit Route
+app.get('/blog/:id/edit', (req, res) => {
+    Article.findById(req.params.id, (err, foundArticle) => {
+        if (err) {
+            console.log(err)
+        } else {
+            // render template with that article
+            res.render("edit", { foundArticle });
+        }
+    })
+})
+
+// Update Route
+app.put('/blog/:id', (req, res) => {
+    Article.findByIdAndUpdate(req.params.id, req.body.foundArticle, (err, updatedBlog) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.redirect("/blog/" + req.params.id)
+        }
+    })
+})
+
+// Show Route - shows more info on one article
+app.get('/blog/:id', (req, res) => {
+    // find article with provided id
+    Article.findById(req.params.id, (err, foundArticle) => {
+        if (err) {
+            console.log(err)
+        } else {
+            // render template with that article
+            res.render("show", { foundArticle });
+        }
+    })
+});
+
 app.get('*', (req, res) => res.render("sorry"))
 
-app.listen(port, () => console.log("go to http://localhost:3000/"))
+app.listen(port, () => console.log("go to http://localhost:3000/blog"))
 
 
 mongoose.connection.once('open', () => console.log("connection has been made")).on('error', () => console.log("connection err", error))
