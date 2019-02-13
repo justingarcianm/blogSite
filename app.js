0c151865cbbe6ddf2db10c36e5bc6d5eba59d174
justin garcia <justinframedev@gmail.com>
2019-02-13 15:49:55 -0700

    replaced app.css with main.css and made new dir

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const express = require("express"),
 
 // seedDB();
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
 app.use(expressSanitizer());
